d8fa0ea31412a4ab55df4f76103355eef475d3ba
devsnest-big <default-big>
2023-07-28 21:48:48 +0800

    modified _app.tsx and ran command prompt:npm --legacy-peer-deps install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,45 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//import libraries
+import '../styles/globals.css';
+import React,{useMemo} from "react";
+import { WalletAdapterNetwork} from "@solana/wallet-adapter-base" ;
+ //walletmodalprovider will open some new screen for user to select wallets?
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui" ;
+import { 
+  ConnectionProvider,
+  WalletProvider
+} from "@solana/wallet-adapter-react";
 
-export default MyApp
+//
+import{ PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+import { clusterApiUrl } from "@solana/web3.js";
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+require("../styles/Home.module.css");
+
+const App = ({Component,pageProps}) => {
+  const network=WalletAdapterNetwork.Devnet
+  //useMemo is a hook that loads stuff only one of the dependencies changes
+  const endpoint=useMemo(() => clusterApiUrl(network),[network]);
+  
+  const wallets=useMemo(() => [ new PhantomWalletAdapter(),],[network]);
+  
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>  
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );//return
+
+};//App
+
+//function MyApp({ Component, pageProps }) {
+//  return (  <Component {...pageProps} /> );
+//}
+
+//export default MyApp;
+export default App;
