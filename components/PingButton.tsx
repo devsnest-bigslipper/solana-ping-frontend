5838f5d3b29ea0b4bb0e4594146d04331ffcebcc
devsnest-big <default-big>
2023-07-30 23:02:00 +0800

    ran ping button in browser and got error: Transaction expired

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,9 +1,10 @@
-import { useConnection,useWallet } from "@solana/wallet-adapter-react";
+import { useConnection,useWallet } from '@solana/wallet-adapter-react';
+import * as web3 from '@solana/web3.js';
 import { FC } from 'react';
-import * as Web3 from "@solana/web3.js";
 import styles from '../styles/Home.module.css';
-const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa");
-const PROGRAM_DATA_ADDRESS = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod");
+
+const PROGRAM_ID = new web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa");
+const PROGRAM_DATA_ADDRESS = new web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod");
 
 
 export const PingButton: FC = () => {
@@ -22,7 +23,7 @@ export const PingButton: FC = () => {
       const instruction = new web3.TransactionInstruction({
         keys: [
           {
-            pubkey : PROGRAM_DATA_PUBLIC_KEY,
+            pubkey : PROGRAM_DATA_ADDRESS,
             isSigner : false,
             isWritable : true,
           },
@@ -32,7 +33,8 @@ export const PingButton: FC = () => {
 
       transaction.add(instruction);
       sendTransaction(transaction,connection).then(sig => {
-          console.log('Explorer URL: https://explorer.solana.com/tx/${sig}?cluster=devnet'); 
+          console.log(sig);
+          //console.log('Explorer URL: https://explorer.solana.com/tx/${sig}?cluster=devnet'); 
       })
     };//onClick
 
